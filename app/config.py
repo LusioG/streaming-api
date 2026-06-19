70b0d6620655edd421d6fe19c2a4c92a4f5eb906
Lusiomp17 <108565399+Lusiomp17@users.noreply.github.com>
2026-06-18 22:54:12 -0300

    Mejoras eliminacion de Boards

--- app/config.py
+++ app/config.py
@@ -1,21 +1,21 @@
 from pydantic_settings import BaseSettings
 
-class Settings(BaseSettings):
 
+class Settings(BaseSettings):
     # Base de datos
     database_url: str
 
     # Seguridad
     secret_key: str
     algorithm: str = "HS256"
-    access_token_expire_minutes: int = 60
+    access_token_expire_minutes: int = 30
 
     # API
     api_title: str = "Streaming API"
     api_version: str = "1.0"
 
     class Config:
-        env_file = ".env"
+        env_file = ".env"  # solo para local
 
 
 settings = Settings()
--- app/database.py
+++ app/database.py
@@ -1,17 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
-from dotenv import load_dotenv
+from app.config import settings
 
-load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not DATABASE_URL:
+if not settings.database_url:
     raise ValueError("DATABASE_URL no está configurada")
 
+
 engine = create_engine(
-    DATABASE_URL,
+    settings.database_url,
     pool_pre_ping=True
 )
 
@@ -22,6 +19,7 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
